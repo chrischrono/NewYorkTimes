984db1406adf6ed6e5d9eab2b9da6437422207c4
Christian Huang <christian@christians-imac.local>
2018-10-08 03:42:26 +0700

    Initial Commit

---
+++ NewYorkTimes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewYorkTimes
+//
+//  Created by Christian  Huang on 08/10/18.
+//  Copyright © 2018 Christian Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
